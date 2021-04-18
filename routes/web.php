c0fdaac3567fbb9b05c74397ff320f2bbe7e9fba
Ayazhan1616 <75525200+Ayazhan1616@users.noreply.github.com>
2021-04-18 18:01:20 +0600

    fdg

--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,11 @@ Route::get('lang/{locale}', [LocalizationController::class, 'index']);
 
 Route::get('skin', [AjaxController::class, 'create']);
 Route::post('skin', [AjaxController::class, 'store']);
-Route::get('manic', [AjaxController::class, 'create']);
+//Route::get('manic', [AjaxController::class, 'create']);
 Route::post('manic', [AjaxController::class, 'store']);
-Route::get('hair', [AjaxController::class, 'create']);
+//Route::get('hair', [AjaxController::class, 'create']);
 Route::post('hair', [AjaxController::class, 'store']);
-Route::get('make', [AjaxController::class, 'create']);
+//Route::get('make', [AjaxController::class, 'create']);
 Route::post('make', [AjaxController::class, 'store']);
 
 Route::get('/main',   [UploadController::class, 'uploadForm']);
